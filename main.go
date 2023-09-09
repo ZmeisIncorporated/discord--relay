b648fabb0a167b32ccc12b8da586e9ee2bb12696
ZmeisIncorporated <zmeisincorporated@mail.ru>
2023-09-09 09:02:53 +0300

    Remove obsolete code, add more console logging

--- main.go
+++ main.go
@@ -8,24 +8,13 @@ import (
 	"os/signal"
 	"syscall"
 
-	log "github.com/sirupsen/logrus"
-
+	"log"
+	
 	"github.com/ZmeisIncorporated/discord--relay/internal/config"
 	"github.com/ZmeisIncorporated/discord--relay/internal/forwarder"
 	"github.com/ZmeisIncorporated/discord--relay/internal/listener"
 )
 
-func init() {
-	var debug = flag.Bool("v", true, "enables verbose logging")
-
-	// Setup logging options
-	if *debug {
-		log.SetLevel(log.DebugLevel)
-	} else {
-		log.SetLevel(log.InfoLevel)
-	}
-	log.SetReportCaller(true)
-}
 
 func main() {
 	var configPath = flag.String("c", "config.yaml", "specicies the path to the config")
@@ -35,49 +24,41 @@ func main() {
 		cancel()
 	}()
 
-	// Parse up the command line flags
 	flag.Parse()
 
-	// Make sure the config exists
 	if _, err := os.Stat(*configPath); os.IsNotExist(err) {
-		log.Errorf("config doesnt exist: %s", err)
-		return
+		log.Fatalf("config doesn't exist: %s", err)
 	}
+	log.Printf("use config: %s", *configPath)
 
-	//Load Config
 	cfg, err := config.NewConfig(*configPath)
 	if err != nil {
-		log.Errorf("Error getting config: %s", err)
-		return
+		log.Fatalf("Error getting config: %s", err)
 	}
 
 	// Connect to the destination for all messages
-	f, err := forwarder.NewForwarder(cfg.Webhooks, log.StandardLogger())
+	f, err := forwarder.NewForwarder(cfg.Webhooks)
 	if err != nil {
-		log.Errorf("Error while creating Forwarder: %s", err)
-		return
+		log.Fatalf("Error while creating Forwarder: %s", err)
 	}
 
 	// Test that we can send to this correctly
 	err = f.Send("[log]", "Forwarder Connected")
 	if err != nil {
-		log.Errorf("Error while sending to log: %s", err)
-		return
+		log.Fatalf("Error while sending to forwarders: %s", err)
 	}
 
 	// Open up pidgin logs listener
 	pidgin, err := listener.NewPidginListener(f, cfg.Logs)
 	if err != nil {
-		log.Errorf("Error while sending to log: %s", err)
-		return
+		log.Fatalf("Error while sending to log: %s", err)
 	}
 	pidgin.Run()
+	log.Printf("Relay is now running.  Press CTRL-C to exit.")
 
-	log.Infoln("Relay is now running.  Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	oscall := <-sc
-	log.Infof("Shutting down from: %+v", oscall)
-	cancel()
+	log.Printf("Shutting down from: %+v", oscall)
 
 }
